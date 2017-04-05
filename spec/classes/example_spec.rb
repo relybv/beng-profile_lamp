3bb25889baf5ecd16adb88c2fb2ef45c4badf78f
P. Gomersbach <p.gomersbach@rely.nl>
2017-04-05 09:55:35 +0000

    add RSpec tests

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -20,11 +20,27 @@ describe 'profile_lamp' do
           it { is_expected.to contain_class('profile_lamp::install') }
           it { is_expected.to contain_class('profile_lamp::config') }
           it { is_expected.to contain_class('profile_lamp::service') }
+          it { is_expected.to contain_class('profile_lamp::prev4') }
+          it { is_expected.to contain_class('profile_lamp::postv4') }
 
           it { is_expected.to contain_class('epel') }
           it { is_expected.to contain_class('apache') }
           it { is_expected.to contain_class('mysql::server') }
           it { is_expected.to contain_package('php-mssql') }
+
+
+          it { is_expected.to contain_firewall('000 accept all icmp') }
+          it { is_expected.to contain_firewall('001 accept all to lo interface') }
+          it { is_expected.to contain_firewall('002 accept related established rules') }
+          it { is_expected.to contain_firewall('003 allow ssh access') }
+
+          it { is_expected.to contain_firewall('100 allow http and https access') }
+
+          it { is_expected.to contain_firewall('900 log all drop connections') }
+          it { is_expected.to contain_firewall('950 drop udp') }
+          it { is_expected.to contain_firewall('951 drop tcp') }
+          it { is_expected.to contain_firewall('952 drop icmp') }
+          it { is_expected.to contain_firewall('999 drop everything else - this is the failsafe rule') }
 
         end
       end
